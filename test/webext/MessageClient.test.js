26a506d1babd93d8443a71e9520ac3be4ec55e56
Shin'ya Ueoka <ueokande@i-beam.org>
2019-03-30 13:45:16 +0900

    Fix MessageListener and tests

--- test/webext/MessageClient.test.js
+++ test/webext/MessageClient.test.js
@@ -1,30 +1,44 @@
 const MessageClient = require('../../lib/webext/MessageClient');
 const assert = require('power-assert');
-const WritableBuffer = require('./WritableBuffer');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+const uuidv4 = require('uuid/v4');
 
 describe('MessageClient', () => {
+
+  let tmpfile;
   let buffer;
   let client;
 
-  before(() => {
-      buffer = new WritableBuffer();
-      client = new MessageClient(buffer);
-  })
+  beforeEach(() => {
+    tmpfile = path.join(os.tmpdir(), 'MessageClient-' + uuidv4());
+    buffer = fs.createWriteStream(tmpfile);
+    client = new MessageClient(buffer);
+  });
+
+  afterEach(() => {
+    fs.unlinkSync(tmpfile);
+  });
 
   describe('#sendMessage()', () => {
-    it('should writers messages with a header', () => {
+    it('should writers messages with a header', async() => {
       client.sendMessage({ name: 'alice', age: 12})
       client.sendMessage({ name: 'bob', age: 14})
 
-      let len1 = buffer.data.slice(0, 4).readUInt32LE();
-      let data1 = buffer.data.slice(4, 4 + len1);
+      buffer.close();
+      await new Promise((resolve) => buffer.on('finish', resolve))
+
+      let raw = fs.readFileSync(tmpfile);
+      let len1 = raw.slice(0, 4).readUInt32LE();
+      let data1 = raw.slice(4, 4 + len1);
       let obj1 = JSON.parse(data1);
 
       assert(obj1.name === 'alice')
       assert(obj1.age === 12)
 
-      let len2 = buffer.data.slice(4 + len1, 4 + len1 + 4).readUInt32LE();
-      let data2 = buffer.data.slice(4 + len1 + 4, 4 + len1 + 4 + len2);
+      let len2 = raw.slice(4 + len1, 4 + len1 + 4).readUInt32LE();
+      let data2 = raw.slice(4 + len1 + 4, 4 + len1 + 4 + len2);
       let obj2 = JSON.parse(data2);
 
       assert(obj2.name === 'bob')
--- test/webext/MessageListener.test.js
+++ test/webext/MessageListener.test.js
@@ -1,53 +1,76 @@
 const MessageListener = require('../../lib/webext/MessageListener');
-const ReadableBuffer = require('./ReadableBuffer');
 const assert = require('power-assert');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+const uuidv4 = require('uuid/v4');
 
 describe('MessageListener', () => {
-  let buffer = new ReadableBuffer();
-  let listener = new MessageListener(buffer);
+
+  let tmpfile;
+
+  const uint32Bytes = (num) => {
+    let bytes = Buffer.alloc(4);
+    bytes.writeUInt32LE(num, 0);
+    return bytes;
+  }
 
   beforeEach(() => {
-    buffer = new ReadableBuffer();
-    listener = new MessageListener(buffer);
+    tmpfile = path.join(os.tmpdir(), 'MessageListener-' + uuidv4());
+  });
+
+  afterEach(() => {
+    fs.unlinkSync(tmpfile);
   });
 
   describe('#onMessage', () => {
-    it('received messages', () => {
+    it('received messages', async () => {
+      let data1 = Buffer.from(JSON.stringify({ name: 'alice', age: 12 }));
+      let data2 = Buffer.from(JSON.stringify({ name: 'bob', age: 14 }));
+      fs.appendFileSync(tmpfile, Buffer.concat([
+        uint32Bytes(data1.length), data1,
+        uint32Bytes(data2.length), data2,
+      ]));
+
       let messages = [];
-
-      listener.onMessage((message) => { messages.push(message); });
+      let buffer = fs.createReadStream(tmpfile);
+      let listener = new MessageListener(buffer);
       listener.listen();
+      await new Promise((resolve) => {
+        let count = 0;
+        listener.onMessage((message) => {
+          messages.push(message);
+          count++;
+          if (count == 2) {
+            resolve();
+          }
+        });
+      });
 
-      let data = Buffer.from(JSON.stringify({ name: 'alice', age: 12 }));
-      let lenBytes = Buffer.alloc(4);
-      lenBytes.writeUInt32LE(data.length, 0);
-      buffer.write(lenBytes);
-      buffer.write(data);
-
-      data = Buffer.from(JSON.stringify({ name: 'bob', age: 14 }));
-      lenBytes.writeUInt32LE(data.length, 0);
-      buffer.write(lenBytes);
-      buffer.write(data);
-
-      assert(messages.length === 2);
+      assert.equal(messages.length, 2);
       assert.deepEqual(messages[0], { name: 'alice', age: 12 })
       assert.deepEqual(messages[1], { name: 'bob', age: 14 })
     });
   });
 
   describe('#onError', () => {
-    it('raises SyntaxError', () => {
-      let errors = [];
-
-      listener.onMessage((message) => {});
-      listener.onError((err) => { errors.push(err) });
-      listener.listen();
-
+    it('raises SyntaxError', async() => {
       let data = Buffer.from('********');
-      let lenBytes = Buffer.alloc(4);
-      lenBytes.writeUInt32LE(data.length, 0);
-      buffer.write(lenBytes);
-      buffer.write(data);
+      fs.appendFileSync(tmpfile, Buffer.concat([
+        uint32Bytes(data.length), data,
+      ]));
+
+      let errors = [];
+      let buffer = fs.createReadStream(tmpfile);
+      let listener = new MessageListener(buffer);
+      listener.listen();
+      listener.onMessage((message) => {});
+      await new Promise((resolve) => {
+        listener.onError((err) => {
+          errors.push(err);
+          resolve();
+        });
+      })
 
       assert(errors.length === 1);
       assert(errors[0] instanceof SyntaxError);
