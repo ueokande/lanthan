bea4ccd073acf0567a864b778e21c2a952707d40
Shin'ya UEOKA <ueokande@i-beam.org>
2019-09-14 09:21:48 +0000

    Load driver's config from default path

--- bin/lanthan-driver.js
+++ bin/lanthan-driver.js
@@ -2,9 +2,6 @@
 
 'use strict';
 
-const pkg = require('../package.json');
-const program = require('commander');
-const path = require('path');
 const os = require('os');
 
 const config = require('../lib/config');
@@ -13,25 +10,14 @@ const MessageListener = require('../lib/driver/MessageListener');
 const Logger = require('../lib/driver/Logger');
 const Server = require('../lib/driver/Server');
 
-const defaultConfigPath = path.join(os.tmpdir(), 'lanthan-driver.json');
-
 const main = () => {
-  program
-    .version(pkg.version)
-    .option('-c, --config <path>', 'path to configuration file',
-      defaultConfigPath)
-    .parse(process.argv);
-
-  let conf = config.loadFrom(program.config);
+  let conf = config.load();
 
   let client = new MessageClient(process.stdout);
   let listener = new MessageListener(process.stdin);
   let logger = new Logger(conf.logFile);
 
-  logger.info(`started with configuration file '${program.config}':`,
-    `address=${conf.address},`,
-    `port=${conf.port},`,
-    `logFile=${conf.logFile}`);
+  logger.info(`started lanthan-driver on ${conf.address}:${conf.port}`);
 
   let server = null;
   try {
