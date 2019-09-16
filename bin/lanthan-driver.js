5c94b98631d04d812afc76ab8783a49625fb2c15
Shin'ya Ueoka <ueokande@i-beam.org>
2019-09-16 11:03:12 +0900

    Migrate bin to TypeScript

--- bin/lanthan-driver.js
+++ bin/lanthan-driver.js
@@ -2,20 +2,18 @@
 
 'use strict';
 
-const os = require('os');
-
-const config = require('../lib/config');
-const MessageClient = require('../lib/driver/MessageClient');
-const MessageListener = require('../lib/driver/MessageListener');
-const Logger = require('../lib/driver/Logger');
-const Server = require('../lib/driver/Server');
+const config = require('../dist/config');
+const { MessageClientImpl } = require('../dist/driver/MessageClient');
+const { MessageListenerImpl } = require('../dist/driver/MessageListener');
+const { FileLogger } = require('../dist/driver/Logger');
+const Server = require('../dist/driver/Server').default;
 
 const main = () => {
   let conf = config.load();
 
-  let client = new MessageClient(process.stdout);
-  let listener = new MessageListener(process.stdin);
-  let logger = new Logger(conf.logFile);
+  let client = new MessageClientImpl(process.stdout);
+  let listener = new MessageListenerImpl(process.stdin);
+  let logger = new FileLogger(conf.logFile);
 
   logger.info(`started lanthan-driver on ${conf.address}:${conf.port}`);
 
@@ -25,7 +23,7 @@ const main = () => {
     server.listen();
   } catch (e) {
     logger.error(e);
-    os.exit(1);
+    process.exit(1);
   }
   process.on('SIGTERM', () => {
     server.close();
